8a31cab993a578adb94c629d1421620064362c35
nickleus <moricet.nicolas@free.fr>
2014-10-04 19:51:37 +0200

    Generate dist files

--- dist/angular-database.js
+++ dist/angular-database.js
@@ -7,17 +7,40 @@ database.factory('$database', Array(function() {
   return new (Database = (function() {
     function Database() {}
 
-    Database.prototype.sql = null;
+    Database.prototype.sql = {};
 
     Database.prototype.createTable = function() {};
 
-    Database.prototype.select = function() {};
+    Database.prototype.select = function(param) {
+      if (typeof param === 'undefined') {
+        param = ['*'];
+      }
+      this.sql['SELECT'] = param;
+      return this;
+    };
 
-    Database.prototype.insert = function() {};
+    Database.prototype.insert = function() {
+      return this;
+    };
 
-    Database.prototype.update = function() {};
+    Database.prototype.update = function() {
+      return this;
+    };
 
-    Database.prototype.where = function() {};
+    Database.prototype.where = function(field, value, operator) {
+      if (typeof operator === 'undefined') {
+        operator = '=';
+      }
+      this.sql['WHERE'] = field + ' ' + operator + ' \'' + value + '\'';
+      return this;
+    };
+
+    Database.prototype.from = function(param) {
+      this.sql['FROM'] = param;
+      return this;
+    };
+
+    Database.prototype.exec = function() {};
 
     return Database;
 
