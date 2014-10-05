59da1b67dc7ae70b7f3c170d28c8b97579e2c503
Nickleus73 <moricet.nicolas@free.fr>
2014-10-05 17:50:42 +0200

    Generate sources file of sql.js adapter

--- dist/adapters/sqljs.js
+++ dist/adapters/sqljs.js
@@ -1,14 +1,37 @@
 var SQLAdapter;
 
 SQLAdapter = (function() {
+  SQLAdapter.prototype.db = null;
+
+  SQLAdapter.prototype.stmt = null;
+
   function SQLAdapter() {
-    console.log('SQLAdapter');
+    this.db = new SQL.Database();
     return;
   }
 
   SQLAdapter.prototype.openDatabase = function() {};
 
-  SQLAdapter.prototype.exec = function() {};
+  SQLAdapter.prototype.exec = function(sql) {
+    console.log(sql);
+    this.db.run(sql);
+    return this;
+  };
+
+  SQLAdapter.prototype.prepare = function(sql) {
+    console.log(sql);
+    this.stmt = this.db.prepare(sql);
+    return this;
+  };
+
+  SQLAdapter.prototype.getResult = function() {
+    var rows;
+    rows = [];
+    while (this.stmt.step()) {
+      rows.push(this.stmt.getAsObject());
+    }
+    return rows;
+  };
 
   return SQLAdapter;
 
